6affc562276b7f02dcb80433a015d34108a26872
Sean C Davis <scdavis41@gmail.com>
2018-03-17 10:55:35 -0400

    Init commit

---
+++ lib/heartwood/service_object.rb
@@ -0,0 +1,7 @@
+require "heartwood/service_object/version"
+
+module Heartwood
+  module ServiceObject
+    # Your code goes here...
+  end
+end
---
+++ lib/heartwood/service_object/version.rb
@@ -0,0 +1,5 @@
+module Heartwood
+  module ServiceObject
+    VERSION = "0.1.0"
+  end
+end
